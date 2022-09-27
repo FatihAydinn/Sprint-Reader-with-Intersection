3fcd65d70685123176321e59ccac1cd09cb114e8
Fatih Aydın <81859164+FatihAydinn@users.noreply.github.com>
2022-09-27 18:09:27 +0300

    Update script.js

--- spritreaderintersection/script.js
+++ spritreaderintersection/script.js
@@ -6,6 +6,7 @@ var ele = document.getElementById("timer");
 document.getElementById("my-button1").style.display = "none";
 document.getElementById("yanlis").style.display = "none";
 document.getElementById("yanlistext2").style.display = "none";
+//------------------------------------
 start.addEventListener("click", function (event) {
   (function () {
     var sec = 1;
@@ -19,21 +20,22 @@ start.addEventListener("click", function (event) {
       document.getElementById("xyz").style.display = "none";
       document.getElementById("yanlis").style.display = "none";
       document.getElementById("yanlistext2").style.display = "none";
-
+//------------------------------------
 
       if (sec >= 5) {
         document.getElementById("timer").style.display = "none";
         document.getElementById("my-button").style.display = "none";
         document.getElementById("my-button").innerText = "TEKRAR";
       }
-      let text = "    belki de hep savaşta gibi hiçbir zaman durmak istemiyor dünyaya bedel eşsiz ruhum dünyayı bilmek istemiyor";
+      let text = "    belki de hep savaşta gibi hiçbir zaman durmak istemiyor dünyaya bedel eşsiz ruhum dünyayı bilmek istemiyor"; // baştan 4 adet boşluk bırakılması gerek
+      
       const chars = text.split(" ");
       for (i = 4; i < sec; i++) {
         document.getElementById("demo").style.display = "inline";
         document.getElementById("demo").innerHTML = chars[i];
 
       }
-      
+     //------------------------------------ 
       count++;
       if (sec > text.split(" ").length) {
         document.getElementById("demo").style.display = "none";
@@ -47,7 +49,7 @@ start.addEventListener("click", function (event) {
     }, 500);
   })();
 });
-
+//------------------------------------
 function pause() {
   clearInterval(timer);
   sec = 0;
@@ -61,7 +63,7 @@ function pause() {
   }
 }
 
-//----
+//------------------------------------
 
 function kontrol() {
 
@@ -71,7 +73,7 @@ function kontrol() {
   var x = (document.getElementById("kontrolText").value).trim();
   document.getElementById("yanlis").style.display = "inline";
   document.getElementById("yanlistext2").style.display = "inline";
-
+//------------------------------------
   var fieldsa = x.split(' ');
   var fieldsb = b.split(' ');
   const intersection = fieldsa.filter(value => fieldsb.includes(value));
@@ -83,6 +85,7 @@ function kontrol() {
   var bos = 0;
   let yanlistext = difference;
   document.getElementById("yanlis").innerHTML = yanlistext;
+  //------------------------------------
   if(girilen<=count)
   {
     bos = count-girilen;
@@ -95,7 +98,6 @@ function kontrol() {
   }
   if(count<(dogru+yanlis))
   {
-
     fazla = (dogru+yanlis)-count
     alert("Girdiğin metindeki doğru sayısı ==> "+dogru+" \nGirdiğin metindeki yanlış veya boş sayısı ==> "+(yanlis)+"\n Fazla kelime girilen kelime sayısı ==>"+fazla+"\n")
   }
@@ -104,3 +106,12 @@ function kontrol() {
   alert("Girdiğin metindeki doğru sayısı ==> "+dogru+" \nGirdiğin metindeki yanlış veya boş sayısı ==> "+(yanlis+bos)+"\n")
   }
 }
+//------------------------------------
+//------------------------------------
+//------------------------------------
+//------------------------------------
+//------------------------------------
+//------------------------------------
+//------------------------------------
+//------------------------------------
+//------------------------------------
